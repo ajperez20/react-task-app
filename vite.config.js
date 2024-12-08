2520ca53a23601f623cabe718786be9c56187234
Arturo Pérez <arjperez31@gmail.com>
2024-12-07 23:40:57 -0400

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ajperez20.github.io/react-task-app",
+  base: "/react-task-app",
 });
